e21e08df023755e5453a592c5a5c6c3c3b9a73e2
Alfonso <>
2020-06-01 18:13:36 -0400

    config

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const path = require('path');
-
+// const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Panaderia'));
+app.use(express.static('./dist/panaderia'));
 
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/Panaderia/index.html'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', { root: 'dist/panaderia' });
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080).on('connection', () => {
+    console.log('Escuchando')
+});
