81bf072149aa2f9324d7c0c9630440cad8012fba
Alfonso <>
2020-06-01 17:14:35 -0400

    configuracion

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/Panaderia'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/Panaderia/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/Panaderia/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
